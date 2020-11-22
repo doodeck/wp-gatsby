1efecbd0c2135890815c73759c6de07d684aec23
doodeck <8515450+doodeck@users.noreply.github.com>
2020-11-22 11:57:48 +0000

    endpoint modified

--- gatsby-config.js
+++ gatsby-config.js
@@ -28,7 +28,8 @@ module.exports = {
         // the only required plugin option for WordPress is the GraphQL url.
         url:
           process.env.WPGRAPHQL_URL ||
-          `https://gatsbypreview.wpengine.com/graphql`,
+          `http://150.136.132.205/graphql`,
+          // `https://gatsbypreview.wpengine.com/graphql`,
       },
     },
 
